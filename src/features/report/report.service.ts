eb75dc80ccb0d58a7968975121db3eb77c9db545
arispelgremory <hiimyino0503@gmail.com>
2026-02-05 09:22:48 +0800

    feat(report): add region field to InvoiceSummaryRow and update mock data accordingly

--- src/features/report/report.service.ts
+++ src/features/report/report.service.ts
@@ -21,6 +21,7 @@ export interface InvoiceSummaryRow {
   poNumber: string;
   outlet: string;
   expectedArrivalDate: string;
+  region: string;
   ctn: number;
 }
 
@@ -33,27 +34,27 @@ const MOVEMENT_MOCK_ROWS: MovementReportRow[] = [
 ];
 
 const INVOICE_SUMMARY_MOCK_ROWS: InvoiceSummaryRow[] = [
-  { poNumber: '#PO260170528', outlet: 'Aeon Midtown Falim', expectedArrivalDate: '22/1/2026', ctn: 10 },
-  { poNumber: '#PO260173297', outlet: 'Lotuss Taiping', expectedArrivalDate: '22/1/2026', ctn: 8 },
-  { poNumber: '#PO260173298', outlet: 'Gurney Paragon', expectedArrivalDate: '22/1/2026', ctn: 5 },
-  { poNumber: '#PO260173299', outlet: 'Amanjaya', expectedArrivalDate: '22/1/2026', ctn: 24 },
-  { poNumber: '#PO260173300', outlet: 'AEON Bukit Mertajam', expectedArrivalDate: '22/1/2026', ctn: 18 },
-  { poNumber: '#PO260173301', outlet: 'Aman Central LG', expectedArrivalDate: '22/1/2026', ctn: 15 },
-  { poNumber: '#PO260173302', outlet: 'Lotuss Teluk Intan', expectedArrivalDate: '22/1/2026', ctn: 9 },
-  { poNumber: '#PO260173303', outlet: 'Pearl City', expectedArrivalDate: '22/1/2026', ctn: 13 },
-  { poNumber: '#PO260173304', outlet: 'Nibong Tebal', expectedArrivalDate: '22/1/2026', ctn: 6 },
-  { poNumber: '#PO260173305', outlet: 'Lotuss Ipoh Bercham', expectedArrivalDate: '22/1/2026', ctn: 24 },
-  { poNumber: '#PO260173306', outlet: 'Sunway Carnival', expectedArrivalDate: '22/1/2026', ctn: 5 },
-  { poNumber: '#PO260173307', outlet: 'Sentra Mall', expectedArrivalDate: '22/1/2026', ctn: 8 },
-  { poNumber: '#PO260173308', outlet: 'AEON Seri Manjung', expectedArrivalDate: '22/1/2026', ctn: 6 },
-  { poNumber: '#PO260173309', outlet: 'AEON Taiping', expectedArrivalDate: '22/1/2026', ctn: 6 },
-  { poNumber: '#PO260173310', outlet: 'AEON Ipoh S18', expectedArrivalDate: '22/1/2026', ctn: 13 },
-  { poNumber: '#PO260173311', outlet: '1st Avenue', expectedArrivalDate: '22/1/2026', ctn: 17 },
-  { poNumber: '#PO260173312', outlet: 'Queensbay Mall', expectedArrivalDate: '22/1/2026', ctn: 10 },
-  { poNumber: '#PO260173313', outlet: 'Mydin Bukit Mertajam', expectedArrivalDate: '22/1/2026', ctn: 12 },
-  { poNumber: '#PO260173314', outlet: 'Gurney Plaza', expectedArrivalDate: '22/1/2026', ctn: 13 },
-  { poNumber: '#PO260173315', outlet: 'Serai Wangi', expectedArrivalDate: '22/1/2026', ctn: 10 },
-  { poNumber: '#PO260173316', outlet: 'AEON Kinta City', expectedArrivalDate: '22/1/2026', ctn: 12 },
+  { poNumber: '#PO260170528', outlet: 'Aeon Midtown Falim', region: "Klang Valley" ,expectedArrivalDate: '22/1/2026', ctn: 10 },
+  { poNumber: '#PO260173297', outlet: 'Lotuss Taiping', region: "Klang Valley" ,expectedArrivalDate: '22/1/2026', ctn: 8 },
+  { poNumber: '#PO260173298', outlet: 'Gurney Paragon', region: "Klang Valley" ,expectedArrivalDate: '22/1/2026', ctn: 5 },
+  { poNumber: '#PO260173299', outlet: 'Amanjaya', region: "Klang Valley" ,expectedArrivalDate: '22/1/2026', ctn: 24 },
+  { poNumber: '#PO260173300', outlet: 'AEON Bukit Mertajam', region: "Klang Valley" ,expectedArrivalDate: '22/1/2026', ctn: 18 },
+  { poNumber: '#PO260173301', outlet: 'Aman Central LG', region: "Klang Valley" ,expectedArrivalDate: '22/1/2026', ctn: 15 },
+  { poNumber: '#PO260173302', outlet: 'Lotuss Teluk Intan', region: "Klang Valley" ,expectedArrivalDate: '22/1/2026', ctn: 9 },
+  { poNumber: '#PO260173303', outlet: 'Pearl City', region: "North" ,expectedArrivalDate: '22/1/2026', ctn: 13 },
+  { poNumber: '#PO260173304', outlet: 'Nibong Tebal', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 6 },
+  { poNumber: '#PO260173305', outlet: 'Lotuss Ipoh Bercham', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 24 },
+  { poNumber: '#PO260173306', outlet: 'Sunway Carnival', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 5 },
+  { poNumber: '#PO260173307', outlet: 'Sentra Mall', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 8 },
+  { poNumber: '#PO260173308', outlet: 'AEON Seri Manjung', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 6 },
+  { poNumber: '#PO260173309', outlet: 'AEON Taiping', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 6 },
+  { poNumber: '#PO260173310', outlet: 'AEON Ipoh S18', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 13 },
+  { poNumber: '#PO260173311', outlet: '1st Avenue', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 17 },
+  { poNumber: '#PO260173312', outlet: 'Queensbay Mall', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 10 },
+  { poNumber: '#PO260173313', outlet: 'Mydin Bukit Mertajam', region: "North" ,expectedArrivalDate: '22/1/2026', ctn: 12 },
+  { poNumber: '#PO260173314', outlet: 'Gurney Plaza', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 13 },
+  { poNumber: '#PO260173315', outlet: 'Serai Wangi', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 10 },
+  { poNumber: '#PO260173316', outlet: 'AEON Kinta City', region: "South" ,expectedArrivalDate: '22/1/2026', ctn: 12 },
 ];
 
 /**
@@ -132,20 +133,21 @@ export function generateInvoiceSummaryPdf(
     'Purchase Order',
     r.poNumber,
     r.outlet,
+    r.region,
     r.expectedArrivalDate,
     String(r.ctn),
   ]);
-  body.push(['', '', '', 'Total', String(totalCtn)]);
+  body.push(['', '', '', '', 'Total', String(totalCtn)]);
 
   autoTable(doc, {
-    head: [['PONumber', 'PO Number', 'Outlet', 'Expected Arrival Date', 'Ctn']],
+    head: [['PONumber', 'PO Number', 'Outlet', 'Region', 'Expected Arrival Date', 'Ctn']],
     body,
     startY: 22,
     theme: 'grid',
     headStyles: { fontStyle: 'bold', fillColor: "black" },
     bodyStyles: { fontSize: 10 },
     columnStyles: {
-      4: { halign: 'right' },
+      5: { halign: 'right' },
     },
     didParseCell: (data: CellHookData) => {
       const raw = data.row.raw as (string | number)[];
