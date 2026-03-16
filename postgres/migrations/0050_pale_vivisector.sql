8d5740aa181c1a8a8725676e2b57d835428d798b
arispelgremory <hiimyino0503@gmail.com>
2026-03-16 16:11:19 +0800

    feat(invoice): add conditional do_no column and populate from delivery_orders
    - Modified the `invoices` table to add the `do_no` column only if it does not already exist.
    - Implemented an update to populate the `do_no` field with corresponding `delivery_order_no` from the `delivery_orders` table based on matching purchase order and organization IDs, ensuring data consistency.

--- postgres/migrations/0050_pale_vivisector.sql
+++ postgres/migrations/0050_pale_vivisector.sql
@@ -1,1 +1,9 @@
-ALTER TABLE "main"."invoices" ADD COLUMN "do_no" text;
+ALTER TABLE "main"."invoices" ADD COLUMN IF NOT EXISTS "do_no" text;
+
+UPDATE "main"."invoices" AS i
+SET "do_no" = d."delivery_order_no"
+FROM "main"."delivery_orders" AS d
+WHERE
+  d."purchase_order_id" = i."po_id"
+  AND d."organization_id" = i."organization_id"
+  AND (i."do_no" IS NULL OR i."do_no" = '');
