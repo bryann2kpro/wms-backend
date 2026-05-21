010e0f907a2afb14ffeff74e2d1471c24059eff3
arispelgremory <hiimyino0503@gmail.com>
2026-05-21 09:22:01 +0800

    fix(migrations): update putaway column addition to be conditional
    - Modified the `lot_no` column addition in the `putaway` table to use `IF NOT EXISTS` to prevent errors if the column already exists.
    - Updated timestamps in the migration journal for consistency.

--- postgres/migrations/0090_faulty_hydra.sql
+++ postgres/migrations/0090_faulty_hydra.sql
@@ -1,3 +1,3 @@
 ALTER TABLE "main"."m_skus" ADD COLUMN "is_lot_controlled" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "main"."m_skus" ADD COLUMN "is_expiry_controlled" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "main"."putaway" ADD COLUMN "lot_no" text;
+ALTER TABLE "main"."putaway" ADD COLUMN IF NOT EXISTS "lot_no" text;
