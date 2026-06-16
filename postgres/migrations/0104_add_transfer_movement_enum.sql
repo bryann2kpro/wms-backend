dc979b7c5de8e74b2fda6930c892a5cf868d2f82
arispelgremory <arispelgremory@gmail.com>
2026-06-16 08:44:06 +0800

    chore(migrations): migration generated contents

---
+++ postgres/migrations/0104_add_transfer_movement_enum.sql
@@ -0,0 +1,2 @@
+ALTER TYPE "main"."inventory_movement_type" ADD VALUE IF NOT EXISTS 'TRANSFER_OUT';--> statement-breakpoint
+ALTER TYPE "main"."inventory_movement_type" ADD VALUE IF NOT EXISTS 'TRANSFER_IN';
